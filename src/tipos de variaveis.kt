79f836d63a06669180b05466fbbea880608365b0
Natasha Queiroz <159721737+natasha-queiroz@users.noreply.github.com>
2024-10-21 23:20:28 -0300

    Atualizar o tipos de variaveis.kt
    atualização

--- src/tipos de variaveis.kt
+++ src/tipos de variaveis.kt
@@ -1,15 +1,15 @@
 fun main() {
-    var x: Int = 18//variavel mutavel
-    val y: Int //variavel imutavel
-    var z: Float = 10.05f
-    var s: Double = 3.18445
-    var a: Long = 101001010001
-    var d: Char = 'f'
-    var c: String = "ana"
-    var v: Short = 100
-    var f: Byte = 0b1010
-    var g: Byte 
-    var i: UInt = 12u// positivo
+    var i: Int = 18//variavel mutavel
+    val x: Int //variavel imutavel
+    var g: Float = 10.05f
+    var c: Double = 3.18445
+    var d: Long = 101001010001
+    var h: Char = 'f'
+    var k: String = "ana"
+    var n: Short = 100
+    var m: Byte = 0b1010
+    var a: Byte 
+    var e: UInt = 12u// positivo
 
     println(x)
     println(y)
