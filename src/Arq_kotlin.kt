e51ea2dbff4cfaa17b0a01cfbff85d2ed488e49a
Natasha Queiroz <natashaqueiroz1612@gmail.com>
2024-10-23 23:14:20 -0300

    arq kotlin

--- src/Arq_kotlin.kt
+++ src/Arq_kotlin.kt
@@ -1,3 +1,27 @@
 fun main() {
     println("Me chamo Natasha e tenho 25 anos")
+
+    var i: Int = 18//variavel mutavel
+    val x: Int //variavel imutavel
+    var g: Float = 10.05f
+    var c: Double = 3.18445
+    var d: Long = 101001010001
+    var h: Char = 'f'
+    var k: String = "ana"
+    var n: Short = 100
+    var m: Byte = 0b1010
+    var a: Byte 
+    var e: UInt = 12u// positivo
+
+    println(i)
+    println(x)
+    println(g)
+    println(c)
+    println(d)
+    println(h)
+    println(k)
+    println(n)
+    println(m)
+    println(a)
+    println(e)
 }
