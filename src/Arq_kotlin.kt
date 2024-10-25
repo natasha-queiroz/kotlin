63e72415ba19492f18cf10c8b82680ee61d1c41a
Natasha Queiroz <natashaqueiroz1612@gmail.com>
2024-10-24 23:32:28 -0300

    variaveis alteradas

--- src/Arq_kotlin.kt
+++ src/Arq_kotlin.kt
@@ -3,27 +3,6 @@ fun main() {
 
 //adicionei esse conteudo de baixo
 
-    var i: Int = 18//variavel mutavel
-    val x: Int //variavel imutavel
-    var g: Float = 10.05f
-    var c: Double = 3.18445
-    var d: Long = 101001010001
-    var h: Char = 'f'
-    var k: String = "ana"
-    var n: Short = 100
-    var m: Byte = 0b1010
-    var a: Byte 
-    var e: UInt = 12u// positivo
+    
 
-    println(i)
-    println(x)
-    println(g)
-    println(c)
-    println(d)
-    println(h)
-    println(k)
-    println(n)
-    println(m)
-    println(a)
-    println(e)
 }
