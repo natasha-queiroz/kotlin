412005674dd21fc0c1b3c73e4b438921678380c3
Natasha Queiroz <159721737+natasha-queiroz@users.noreply.github.com>
2024-12-08 21:13:00 -0300

    Atualizar o tipos de variaveis.kt

--- src/tipos de variaveis.kt
+++ src/tipos de variaveis.kt
@@ -1,10 +1,10 @@
 fun main() 
 
 {
-    var a: Int = 18 //variavel mutavel
-    val b: Int //variavel imutavel
+    var l: Int = 18 //variavel mutavel
+    val m: Int //variavel imutavel
     var c: Float = 10.05f
-    var d: Double = 3.18445
+    var n: Double = 3.18445
     var e: Long = 101001010001
     var f: Char = 'f'
     var g: String = "ana"
